4646e3fc25373e3b8248f8482aac3cf68615c01c
jakubmraz <jakub.mraz111@gmail.com>
2024-11-26 12:42:48 +0100

    Add dummy dagger pipeline into main.go

--- main.go
+++ main.go
@@ -16,6 +16,15 @@ func main() {
 	}
 	defer client.Close()
 
-	// Placeholder for actual stuff we add later
-	log.Println("Hello world!")
+	// Dummy pipeline: Run "echo Hello World!" in an Alpine container
+	output, err := client.Container().
+		From("alpine").
+		WithExec([]string{"echo", "Hello World! John Dagger here."}).
+		Stdout(ctx)
+	if err != nil {
+		log.Fatalf("Failed to run pipeline: %v", err)
+	}
+
+	// Print pipeline output
+	log.Println("Pipeline output:", output)
 }
