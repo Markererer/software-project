42f79de9d941c184ddf32ec92378c14aa3d7f5e9
jakubmraz <58209778+jakubmraz@users.noreply.github.com>
2024-11-29 15:52:40 +0100

    Add feature extraction to the dagger pipeline (#5)
    * Split off a features.py file, formatted, added to pipeline
    
    * Use only one python container
    
    * Fixed all instances of misspelling interim
    
    * Force unbuffered output with script calls
    
    * Split runs again
    
    * Use updated containers after each execute

--- scripts/features.py
+++ scripts/features.py
@@ -1,29 +1,57 @@
-from pathlib import Path
+import os
+import pandas as pd
+import warnings
+import argparse
+import pickle #🥒
 
-import typer
-from loguru import logger
-from tqdm import tqdm
+# Set up warnings and pandas options
+warnings.filterwarnings('ignore')
+pd.set_option('display.float_format', lambda x: "%.3f" % x)
 
-from sdw_project.config import PROCESSED_DATA_DIR
+def create_dummy_cols(df, col):
+    df_dummies = pd.get_dummies(df[col], prefix=col, drop_first=True)
+    new_df = pd.concat([df, df_dummies], axis=1)
+    new_df = new_df.drop(col, axis=1)
+    return new_df
 
-app = typer.Typer()
+def main(args):
+    # Read the data
+    data = pd.read_csv(os.path.join(args.processed_data_dir, "train_data_gold.csv"))
 
+    # Process the data
+    data = data.drop(["lead_id", "customer_code", "date_part"], axis=1)
+    cat_cols = ["customer_group", "onboarding", "bin_source", "source"]
+    cat_vars = data[cat_cols]
+    other_vars = data.drop(cat_cols, axis=1)
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Features generation complete.")
-    # -----------------------------------------
+    for col in cat_vars:
+        cat_vars[col] = cat_vars[col].astype("category")
+        cat_vars = create_dummy_cols(cat_vars, col)
 
+    data = pd.concat([other_vars, cat_vars], axis=1)
+
+    for col in data:
+        data[col] = data[col].astype("float64")
+
+    print("Features extracted.")        
+
+    # Instantiate feature space
+    y = data["lead_indicator"]
+    X = data.drop(["lead_indicator"], axis=1)
+
+    # Save as a 🥒 file to pass between scripts
+    with open(args.interim_data_dir + "/X.pkl", "wb") as f:
+        pickle.dump(X, f)
+    with open(args.interim_data_dir + "/y.pkl", "wb") as f:
+        pickle.dump(y, f)
+
+    print("Training and test data saved into the interim directory.")
 
 if __name__ == "__main__":
-    app()
+    parser = argparse.ArgumentParser(description="Data Preprocessing Script")
+    parser.add_argument("--raw_data_dir", type=str, required=True, help="Path to the raw data directory")
+    parser.add_argument("--interim_data_dir", type=str, required=True, help="Path to save interim data")
+    parser.add_argument("--processed_data_dir", type=str, required=True, help="Path to save processed data")
+
+    args = parser.parse_args()
+    main(args)
