84171c97e09c0d6bae128f75c5252f967a9aa7b7
jakubmraz <jakub.mraz111@gmail.com>
2024-11-29 15:37:52 +0100

    Use updated containers after each execute

--- main.go
+++ main.go
@@ -38,33 +38,35 @@ func main() {
 		WithWorkdir("/app").
 		WithExec([]string{"pip", "install", "-r", "requirements.txt"})
 
-	// Run the preprocessing script
-	output, err := pythonContainer.
-		WithExec([]string{
-			"python", "-u", preprocessingScriptPath,
-			"--raw_data_dir", rawDataDir,
-			"--artifacts_dir", artifactsDir,
-			"--processed_data_dir", processedDataDir,
-			"--min_date", minDate,
-			"--max_date", maxDate,
-		}).
-		Stdout(ctx)
+	// Run the preprocessing script and get the updated container
+	preprocessingContainer := pythonContainer.WithExec([]string{
+		"python", "-u", preprocessingScriptPath,
+		"--raw_data_dir", rawDataDir,
+		"--artifacts_dir", artifactsDir,
+		"--processed_data_dir", processedDataDir,
+		"--min_date", minDate,
+		"--max_date", maxDate,
+	})
+
+	// Get the output from the preprocessing script
+	output, err := preprocessingContainer.Stdout(ctx)
 	if err != nil {
-		log.Fatalf("Failed to run scripts: %v", err)
+		log.Fatalf("Failed to run preprocessing script: %v", err)
 	}
 	log.Println("Preprocessing output:", output)
 
-	// Run the feature extraction script
-	output, err = pythonContainer.
-		WithExec([]string{
-			"python", "-u", featuresScriptPath,
-			"--raw_data_dir", rawDataDir,
-			"--interim_data_dir", interimDir,
-			"--processed_data_dir", processedDataDir,
-		}).
-		Stdout(ctx)
+	// Run the feature extraction script on the updated container
+	featuresContainer := preprocessingContainer.WithExec([]string{
+		"python", "-u", featuresScriptPath,
+		"--raw_data_dir", rawDataDir,
+		"--interim_data_dir", interimDir,
+		"--processed_data_dir", processedDataDir,
+	})
+
+	// Get the output from the feature extraction script
+	output, err = featuresContainer.Stdout(ctx)
 	if err != nil {
-		log.Fatalf("Failed to run scripts: %v", err)
+		log.Fatalf("Failed to run features script: %v", err)
 	}
 	log.Println("Feature extraction output:", output)
 }
