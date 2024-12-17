558b2a92522eac9169fcbba5a4c7a156975afd62
jakubmraz <jakub.mraz111@gmail.com>
2024-12-17 21:18:50 +0100

    Update get_data.py

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,6 +1,7 @@
 import subprocess
 
 def main():
+    subprocess.run(["dvc", "update", "./data/raw/raw_data.csv.dvc"], check=True)
     subprocess.run(["dvc", "pull"], check=True)
 
     print(f"DVC Pull success!")
