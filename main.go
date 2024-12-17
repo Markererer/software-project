2ac146f197b26534e955caca794210b6b3357473
jakubmraz <jakub.mraz111@gmail.com>
2024-12-17 21:27:58 +0100

    Made path into an arg for download script

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func main() {
 	// Download the training data from Jeppe Theiss Kristensen's GitHub repo
 	rawDataContainer := pythonContainer.WithExec([]string{
 		"python", "-u", downloadDataScriptPath,
+		"--raw_data_dir", rawDataDir,
 	})
 
 	// Get the output from the preprocessing script
--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,10 +1,16 @@
 import subprocess
+import argparse
+import os
 
-def main():
-    subprocess.run(["dvc", "update", "./data/raw/raw_data.csv.dvc"], check=True)
+def main(args):
+    subprocess.run(["dvc", "update", os.path.join(args.raw_data_dir, "raw_data.csv.dvc")], check=True)
     subprocess.run(["dvc", "pull"], check=True)
 
     print(f"DVC Pull success!")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Data Downloading Script")
+    parser.add_argument("--raw_data_dir", type=str, required=True, help="Path to the raw data directory")
+
+    args = parser.parse_args()
+    main(args)
